13d7c6324e373805b3cd4d17c8a635234c12af1c
ajlopez <webmaster@ajlopez.com>
2015-06-27 07:18:57 -0300

    First implementation, based on Aktores channels

---
+++ Src/ObjectChannel.Tests/Utilities/TypeUtilitiesTests.cs
@@ -0,0 +1,38 @@
+﻿namespace ObjectChannel.Tests.Utilities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    using ObjectChannel.Utilities;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TypeUtilitiesTests
+    {
+        [TestMethod]
+        public void GetTypeByName()
+        {
+            Type type = TypeUtilities.GetType("System.Int32");
+
+            Assert.IsNotNull(type);
+            Assert.AreEqual(type, typeof(int));
+        }
+
+        [TestMethod]
+        public void GetTypeInAnotherAssembly()
+        {
+            Type type = TypeUtilities.GetType("System.Data.DataSet");
+
+            Assert.IsNotNull(type);
+            Assert.AreEqual(type, typeof(System.Data.DataSet));
+        }
+
+        [TestMethod]
+        public void NullIfUnknownType()
+        {
+            Assert.IsNull(TypeUtilities.GetType("Foo.Bar"));
+        }
+    }
+}
---
+++ Src/ObjectChannel/Types.cs
@@ -0,0 +1,23 @@
+﻿namespace ObjectChannel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public enum Types
+    {
+        Null,
+        Byte,
+        Character,
+        Short,
+        Integer,
+        Long,
+        Single,
+        Double,
+        Decimal,
+        String,
+        Object,
+        Type
+    }
+}
